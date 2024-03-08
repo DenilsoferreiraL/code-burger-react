6e21888629274d67ab3c365cc05f9dacc1e2bdd9
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-03-08 11:50:31 -0300

    chore: registration page created.

---
+++ src/containers/Registration/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {
+    Container
+} from './styles'
+function Registration() {
+    return (
+        <Container>
+            <h1>Olá</h1>
+        </Container>
+    )
+}
+
+export default Registration
