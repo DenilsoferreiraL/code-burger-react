851bce157e13b1350861ad78f33ad3778039b4a3
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-04-12 09:11:17 -0300

    Feat (CartResume): Summary of the finished cart, index structure and style using the display grid.

--- src/components/CartResume/index.js
+++ src/components/CartResume/index.js
@@ -1,12 +1,28 @@
 import React from 'react'
 
 import { Container } from './styles'
+import { Button } from '../Button'
 
 export function CartResume() {
     return (
-        <Container>
-            <h1>Olá</h1>
-        </Container>
+        <div>
+            <Container>
+                <div className='container-top'>
+                    <h2 className='title'>Resumo do Pedido</h2>
+                    <p className='itens'>Itens</p>
+                    <p className='items-price'>R$ 10,00</p>
+                    <p className='delivery-tax'>Taxa de entrega</p>
+                    <p className='delivery-tax-price'>R$ 10,00</p>
+                </div>
+                <div className='container-bottom'>
+                    <p>Total</p>
+                    <p>R$ 20,00 </p>
+                </div>
+            </Container>
+            <Button style={{ width: '100%', marginTop: 30 }}>
+                Finalizar Pedido
+            </Button>
+        </div>
     )
 }
 
