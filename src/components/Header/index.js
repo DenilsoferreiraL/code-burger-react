9c7f9c505cf501f04f3cc87a8bf2fbde8400e396
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-04-15 15:27:24 -0300

    Feat (Header) : Used to manipulate the history when the Header component is clicked, giving access to the next page.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,38 +2,41 @@ import React from 'react'
 
 import Person from '../../assets/user.svg'
 import Cart from '../../assets/cart.svg'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 
 import {
-    Container, ContainerLeft, PageLink, ContainerRight, ContainerText
+    Container, ContainerLeft, PageLink, Line, ContainerRight, PageLinkExit, ContainerText
 
 } from './styles'
 
 export function Header() {
+    const { push, location: { pathname } } = useHistory()
+
     return (
         <Container>
             <ContainerLeft>
-                <PageLink>
+                <PageLink onClick={() => push('/')} isActive={pathname === '/'}>
                     Home
                 </PageLink>
-                <PageLink>
+                <PageLink onClick={() => push('/produtos')} isActive={pathname.includes('produtos')}>
                     Ver Produtos
                 </PageLink>
-            </ContainerLeft>
+            </ContainerLeft >
             <ContainerRight>
                 <PageLink>
                     <img src={Cart} alt='Carrinho' />
                 </PageLink>
-                <div></div>
+                <Line></Line>
                 <PageLink>
                     <img src={Person} alt='Usuário' />
                 </PageLink>
                 <ContainerText>
                     <p>Olá, Denilso</p>
-                    <PageLink> Sair</PageLink>
+                    <PageLinkExit> Sair</PageLinkExit>
                 </ContainerText>
             </ContainerRight>
 
-        </Container>
+        </Container >
     )
 }
 
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,19 +1,51 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
+height: 72px;
+background-color: #ffffff;
+box-shadow: 2px 3px 5px 0px rgba(0, 0, 0, 0.15);
+display: flex;
+align-items: center;
+justify-content: space-around;
+flex-direction: row;
 `
 export const ContainerLeft = styled.div`
-
+display: flex;
+gap: 30px;
 `
 export const PageLink = styled.a`
-
+cursor: pointer;
+text-decoration: none;
+color: ${props => props.isActive ? '#FD874C' : '#000'} ;
+font-size: 16px;
+line-height: 19px;
+font-weight:${props => props.isActive ? 'bold' : 'normal'} ;
 `
 export const ContainerRight = styled.div`
-
+display: flex;
+align-items: center;
+gap: 20px;
 `
-export const ContainerText = styled.p`
 
+export const Line = styled.div`
+height: 40px;
+border-right: 0.5px  solid #BABABA;  ;
+`
+export const PageLinkExit = styled.a`
+color: #FD874C;
+font-size: 14px;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+`
+export const ContainerText = styled.div`
+
+ p{
+color: #020202;
+font-size: 14px;
+font-style: normal;
+font-weight: 300;
+ }
 `
 
 
