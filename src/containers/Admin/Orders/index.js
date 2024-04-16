ddf847bba575c9b7eb8aa0eb38ad3c0b69cf28b1
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-04-16 14:13:46 -0300

    Feat (Admin - Orders) : Added the useState hook to manage the status of orders.

--- src/containers/Admin/Orders/index.js
+++ src/containers/Admin/Orders/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../../services/api'
 
 import {
     Container
@@ -6,6 +7,19 @@ import {
 } from './styles'
 
 function Orders() {
+    const [orders, setOrders] = useState([])
+
+    useEffect(() => {
+        async function loadOrders() {
+            const { data } = await api.get('orders')
+
+            console.log(orders)
+            setOrders(data)
+        }
+
+        loadOrders()
+    }, [])
+
     return (
         <Container>
             <h1>Pedidos</h1>
--- src/containers/Admin/Orders/styles.js
+++ src/containers/Admin/Orders/styles.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
+background: #efefef;
+min-height: 100vh;
 `
 // export const CartImg = styled.img`
 
