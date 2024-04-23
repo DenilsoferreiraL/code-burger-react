5b677887c580a58809bac437e8e380fba5cefab6
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-04-23 09:38:35 -0300

    Feat (Admin- SideMenuAdmin): New SideMenuAdmin component responsible for the side menu on the admin screen. A folder called menu-list has been created to store the menu items, including their names, links and icons. The MenuList component exports these items to SideMenuAdmin, where they are mapped using the map function.

---
+++ src/components/SideMenuAdmin/styles.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { Link } from 'react-router-dom'
+
+
+export const Container = styled.div`
+background: #000;
+box-shadow: 0px 0px 14px 0px rgba(0, 0, 0, 0.15);
+width: 300px;
+top:0;
+left: 0;
+
+hr{
+    margin: 50px 15px;
+}
+`
+export const ItemContainer = styled.div`
+display: flex;
+align-items: center;
+padding-left: 20px;
+height: 60px;
+border-radius: 2px;
+background: #565656;
+margin: 18px;
+
+.icon{
+    color: #ffffff;
+}
+`
+
+export const ListLink = styled(Link)`
+color: #ffffff;
+font-size: 16px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+margin-left: 10px;
+text-decoration: none;
+`
--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Orders from './Orders'
+import { SideMenuAdmin } from '../../components/SideMenuAdmin'
 import {
     Container
 
 } from './styles'
 
 export function Admin() {
-   
+
     return (
         <Container>
-           <Orders/>
+            <SideMenuAdmin />
+            <Orders />
         </Container>
     )
 }
