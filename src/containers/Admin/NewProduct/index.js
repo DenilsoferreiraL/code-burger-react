8161b2b5d94ce35034a7deaa8a6fb74066d5d689
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-04-26 15:43:39 -0300

    Feat (NewProduct): Implemented the selection of categories in the form for adding products. The React Hook Form Controller component was used to integrate the selection field with the form state. The “category_id” field is controlled by useForm(), and the Controller is responsible for rendering the ReactSelect component, allowing the user to select a category for the product. The options are taken from the categories state, which is initialized as an empty array. The form is submitted with handleSubmit() when the user submits the data.

--- src/containers/Admin/NewProduct/index.js
+++ src/containers/Admin/NewProduct/index.js
@@ -2,32 +2,33 @@ import React, { useEffect, useState } from 'react'
 
 import api from '../../../services/api'
 import ReactSelect from 'react-select';
-import { useForm, SubmitHandler } from "react-hook-form"
+import { useForm, Controller } from "react-hook-form"
 
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 import {
     Container, Label, Input, ButtonStyles, LabelUpload
 } from './styles'
+import { colors } from '@mui/material';
 
 function NewProduct() {
     const [fileName, setFileName] = useState(null)
-    const { register, handleSubmit } = useForm()
-    const onSubmit = (data) => console.log(data)
+    const [categories, setCategories] = useState([])
+    const { register, handleSubmit, control } = useForm()
+
+    const onSubmit = data => console.log(data)
 
     useEffect(() => {
-        async function loadOrders() {
-            const { data } = await api.get('products');
-
+        async function loadCategories() {
+            const { data } = await api.get('categories');
+            setCategories(data);
         }
-        loadOrders();
-
+        loadCategories()
     }, []);
-
 
     return (
         <Container>
-            <form noValidate>
+            <form noValidate onSubmit={handleSubmit(onSubmit)}>
                 <Label>Nome</Label>
                 <Input type='text'  {...register("name")} />
 
@@ -51,8 +52,22 @@ function NewProduct() {
                         }} />
                 </LabelUpload>
 
-                <ReactSelect />
-
+                <Controller
+                    name="category_id"
+                    control={control}
+                    render={({ field }) => {
+                        return (
+                            <ReactSelect
+                                {...field}
+                                options={categories}
+                                getOptionLabel={cat => cat.name}
+                                getOptionValue={cat => cat.id}
+                                placeholder="Categorias"
+                            />
+                        )
+                    }}
+                >
+                </Controller>
                 <ButtonStyles>Adicionar produtos</ButtonStyles>
             </form>
         </Container >
